ddabf772f29129187559e32f17f917ccd6138fe7
joshuapicciano <picciano.joshua@student.oaklandcc.edu>
2022-05-16 17:51:09 -0400

    Lab01

---
+++ Lab01/src/lab01/Lab01.java
@@ -0,0 +1,13 @@
+package lab01;
+
+public class Lab01 {
+    public static void main(String[] args) {
+        System.out.println("My partner's name is Sapana Pandey.");
+        System.out.println("Sapana's major is computer science at Wayne State University.");
+        System.out.println("Sapana is taking this course to transfer the credit to Wayne State upon completion.");
+        System.out.println("Sapana does have a little bit of programming experience coming into this class.");
+        System.out.println("Some of Sapana's hobbies include traveling, dancing, and playing sports.");
+        System.out.println("Over the summer, Sapana plans to take Calculus 1 along with this course, in addition to working at Starbucks and spending the rest of summer with family.");
+        System.out.println("One fact is that Sapana really loves to spend free time watching movies");
+    }
+}
