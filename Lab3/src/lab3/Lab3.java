2537fb0eb4118f1e1b19abaabe472d368e547aa6
joshuapicciano <picciano.joshua@student.oaklandcc.edu>
2022-05-27 03:32:27 -0400

    Lab3

---
+++ Lab3/src/lab3/Lab3.java
@@ -0,0 +1,47 @@
+package lab3;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Lab3 {
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        NumberFormat numberFormat = NumberFormat.getNumberInstance();
+        numberFormat.setMaximumFractionDigits(3);
+
+        System.out.println("Please enter five numeric values");
+        double firstUserValue = keyboard.nextDouble();
+        double secondUserValue = keyboard.nextDouble();
+        double thirdUserValue = keyboard.nextDouble();
+        double fourthUserValue = keyboard.nextDouble();
+        double fifthUserValue = keyboard.nextDouble();
+
+        double maxOfFirstTwoValues = Math.max(firstUserValue, secondUserValue);
+        double maxOfFirstThreeValues = Math.max(maxOfFirstTwoValues, thirdUserValue);
+        double maxOfFirstFourValues = Math.max(maxOfFirstThreeValues, fourthUserValue);
+        double maxUserValue = Math.max(maxOfFirstFourValues, fifthUserValue);
+
+        double minOfFirstTwoValues = Math.min(firstUserValue, secondUserValue);
+        double minOfFirstThreeValues = Math.min(minOfFirstTwoValues, thirdUserValue);
+        double minOfFirstFourValues = Math.min(minOfFirstThreeValues, fourthUserValue);
+        double minUserValue = Math.min(minOfFirstFourValues, fifthUserValue);
+
+        double averageOfUserValues = (firstUserValue + secondUserValue + thirdUserValue + fourthUserValue + fifthUserValue) / 5;
+
+        double standardDeviationOfUserValues =
+                Math.sqrt((Math.pow(firstUserValue - averageOfUserValues, 2)
+                            + Math.pow(secondUserValue - averageOfUserValues, 2)
+                            + Math.pow(thirdUserValue - averageOfUserValues, 2)
+                            + Math.pow(fourthUserValue - averageOfUserValues, 2)
+                            + Math.pow(fifthUserValue - averageOfUserValues, 2))
+                                                                            / 5);
+
+        System.out.println("The largest number of the five numbers is " + maxUserValue);
+        System.out.println("The smallest number of the five numbers is " + minUserValue);
+        System.out.println("The average of the five numbers is " +averageOfUserValues);
+        System.out.println("The standard deviation of the five numbers is " + standardDeviationOfUserValues);
+
+    }
+}
